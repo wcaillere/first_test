31e6e3e30a021f2804d306b3cacbedac36122d99
william <wcaillere.pro@gmail.com>
2023-03-15 11:53:32 +0100

    Test template twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,21 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
-    #[Route("/hello")]
+    #[Route("/home/hello")]
     public function hello(): Response
     {
         return new Response('<html><body>hello</body></html>');
+    }
+
+    #[Route("/home/twig")]
+    public function testTwig(): Response
+    {
+        return $this->render('testTwig.html.twig', ['word' => "Twig"]);
     }
 }
