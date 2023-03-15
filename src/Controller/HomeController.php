b245d11adf1ee0af7119176fe94c91c003a0be24
william <wcaillere.pro@gmail.com>
2023-03-15 16:57:25 +0100

    mise en groupe des routes Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route("/home")]
 class HomeController extends AbstractController
 {
-    #[Route("/home/hello")]
+    #[Route("/", name: "home_hello")]
     public function hello(): Response
     {
-        return new Response('<html><body>hello</body></html>');
+        return new Response('<html lang="fr"><body>hello</body></html>');
     }
 
-    #[Route("/home/twig")]
+    #[Route("/twig", name: "home_twig")]
     public function testTwig(): Response
     {
         return $this->render('testTwig.html.twig', ['word' => "Twig"]);
