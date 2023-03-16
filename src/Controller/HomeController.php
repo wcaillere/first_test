3788d00f3a1433a792a8a69205e3fe1f8bf25be5
William Caillere <wcaillere.pro@gmail.com>
2023-03-16 12:29:21 +0100

    Test de l'ajout d'exceptions

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route("/home")]
 class HomeController extends AbstractController
 {
-    #[Route("/", name: "home_hello")]
-    public function hello(): Response
+    #[Route("/hello/{name}", name: "home_hello")]
+    public function hello(string $name): Response
     {
-        return new Response('<html lang="fr"><body>hello</body></html>');
+        if ($name == "exception") {
+            throw new Exception("Test de l'exception");
+        }
+        if ($name == "errorPage") {
+            throw $this->createNotFoundException("Test de l'errorPage");
+        }
+        return new Response('<html lang="fr"><body>hello '.$name.'</body></html>');
     }
 
     #[Route("/twig", name: "home_twig")]
